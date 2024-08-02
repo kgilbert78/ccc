5159691ec2b36b609d79a869782406f4c0b41559
Kyle Gilbert <80863489+kgilbert78@users.noreply.github.com>
2024-08-02 12:19:09 -0400

    correct pointer declaration to dereference operator

--- chapter_3/listing_3_2.cpp
+++ chapter_3/listing_3_2.cpp
@@ -2,7 +2,7 @@
 
 int main() {
   int gettysburg{};
-  int* gettysburg_address = &gettysburg;
+  int *gettysburg_address = &gettysburg;
   printf("Value at gettysburg_address: %d\n", *gettysburg_address);
   printf("Gettysburg Address: %p\n", gettysburg_address);
   *gettysburg_address = 17325;
